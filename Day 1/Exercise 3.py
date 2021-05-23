e7bfbceffcb63673d4a84ca96ccd40bae712846c
Andrew Ash <lludu@live.com>
2021-05-23 12:15:47 -0400

    Day 001: Exercise 3, Finding String Length

---
+++ Day 1/Exercise 3.py
@@ -0,0 +1,10 @@
+#This is day 1, practice if you will, with variables, inputs, strings, debugging
+
+#Exercise 3:
+print(len(input('What is your name? ')))
+
+#link to exercise
+# https://replit.com/@Lludu/day-1-3-exercise
+
+#link to exercise checker
+# https://replit.com/@Lludu/day-1-3-test-your-code
