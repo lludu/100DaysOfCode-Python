9d5fc42e3bafdb623926d372d7c36300ef4dba7b
Andrew Ash <lludu@live.com>
2021-05-23 12:15:18 -0400

    Day 001: Exercise 2, Debugging

---
+++ Day 1/Exercise 2.py
@@ -0,0 +1,13 @@
+#This is day 1, practice if you will, with variables, inputs, strings, debugging
+
+#Exercise 2:
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#link to exercise
+# https://replit.com/@Lludu/day-1-2-exercise#main.py
+
+#link to exercise checker
+# https://replit.com/@Lludu/day-1-2-test-your-code
