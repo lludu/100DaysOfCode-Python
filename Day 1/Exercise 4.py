59dfee7884ccbe2812937897a12c6676b2a2bb79
Andrew Ash <lludu@live.com>
2021-05-23 12:16:05 -0400

    Day 001: Exercise 4, Changing Values

---
+++ Day 1/Exercise 4.py
@@ -0,0 +1,20 @@
+#This is day 1, practice if you will, with variables, inputs, strings, debugging
+
+#Exercise 4:
+
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+
+
+
+#Write your code above this line 👆
+
+
+#link to exercise
+# https://replit.com/@Lludu/day-1-4-exercise
+
+#link to exercise checker
+# https://replit.com/@Lludu/day-1-4-test-your-code
