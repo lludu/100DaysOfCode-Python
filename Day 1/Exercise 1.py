7facaf7adc154ed05ec8dacc4d692a17d5bbb988
Andrew Ash <lludu@live.com>
2021-05-23 12:13:49 -0400

    Day 001: Exercise 1, Print Statement

---
+++ Day 1/Exercise 1.py
@@ -0,0 +1,13 @@
+#This is day 1, practice if you will, with variables, inputs, strings, debugging
+
+#Exercise 1:
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-1-1-exercise
+
+#Link to Exercise checker
+# https://replit.com/@Lludu/day-1-1-test-your-code
