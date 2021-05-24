5470518750b571106183c6faea77fad083e139ea
Andrew Ash <lludu@live.com>
2021-05-24 19:52:16 -0400

    Day 002: Exercise 1, Manipulate String, Add 2 numbers

---
+++ Day 2/Exercise 1.py
@@ -0,0 +1,16 @@
+#This is day 2, understanding data types and string Manipulation
+
+#Exercise 1 - Add together the inputted 2 digit number:
+
+one = int(two_digit_number[0])
+two = int(two_digit_number[1])
+
+print(one+two)
+
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-2-1-exercise#main.py
+
+#link to Exercise Checker
+# https://replit.com/@Lludu/day-2-1-test-your-code#main.py
