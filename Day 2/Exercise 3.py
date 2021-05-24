9a90925923bf41978b6a1d53b0fa3eeba6f510c7
Andrew Ash <lludu@live.com>
2021-05-24 19:53:37 -0400

    Day 002: Exercise 3, Your life in weeks, f-strings

---
+++ Day 2/Exercise 3.py
@@ -0,0 +1,21 @@
+#This is day 2, understanding data types and string Manipulation
+
+#Exercise 3 - Your Life in weeks:
+age = 90 - int(age)
+
+months= age*12
+weeks= age*52
+days= age*365
+
+# F STRINGS ARE AWESOME!
+print(f'You have {days} days, {weeks} weeks, and {months} months left.')
+
+
+
+
+
+#link to exercise
+# https://replit.com/@Lludu/day-2-3-exercise#main.py
+
+#link to exercise checker
+# https://replit.com/@Lludu/day-2-3-test-your-code#main.py
