95d036f49f2010521f897821806868fe6bada3d1
Andrew Ash <lludu@live.com>
2021-05-24 19:53:03 -0400

    Day 002: Exercise 2, BMI Calculator

---
+++ Day 2/Exercise 2.py
@@ -0,0 +1,10 @@
+#This is day 2, understanding data types and string Manipulation
+
+#Exercise 2 - BMI Calculator:
+print(round(float(weight)/(float(height)**2)))
+
+#link to exercise
+# https://replit.com/@Lludu/day-2-2-exercise#main.py
+
+#link to exercise checker
+# https://replit.com/@Lludu/day-2-2-test-your-code#main.py
