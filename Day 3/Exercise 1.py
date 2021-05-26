eafd64547c176d64c2132b8f70a6d54c731fc6b0
Andrew Ash <lludu@live.com>
2021-05-25 20:14:13 -0400

    Day 003: Exercise 1, Odd or Even

---
+++ Day 3/Exercise 1.py
@@ -0,0 +1,16 @@
+#This is day 3, Control Flow & Logical operators
+
+#Exercise 1 - Determine odd or even number with modulo %:
+
+if (number % 2 ==0):
+  print('This is an even number.')
+else:
+  print('This is an odd number.')
+
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-3-1-exercise#main.py
+
+#link to Exercise Checker
+# https://replit.com/@Lludu/day-3-1-test-your-code#main.py
