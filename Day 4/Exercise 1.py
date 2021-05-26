756ce4c540a4b63454ea049fe717e26ff3acf740
Andrew Ash <lludu@live.com>
2021-05-26 16:08:32 -0400

    Day 004: Exercise 1, Heads or Tails

---
+++ Day 4/Exercise 1.py
@@ -0,0 +1,18 @@
+#This is day 4, randomization and python lists
+
+#Exercise 1 - Heads or Tails:
+
+import random
+
+toss = random.randint(0,1)
+
+if (toss == 0):
+  print(toss)
+  print('Tails')
+else:
+  print(toss)
+  print('Heads')
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-4-1-exercise#main.py
