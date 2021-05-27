9eea6d0975e1cee38833462545cc340864736342
Andrew Ash <lludu@live.com>
2021-05-27 10:22:23 -0400

    Day 005: Exercise 4, FizzBuzz

---
+++ Day 5/Exercise 4 FizzBuzz.py
@@ -0,0 +1,20 @@
+#This is day 5, python loops
+
+#Exercise 2 - Adding all even numbers between 1 to 100:
+
+
+for number in range(1, 101):
+  if (number % 15 == 0):
+    print('FizzBuzz')
+  elif (number % 3 == 0):
+    print('Fizz')
+  elif (number % 5 == 0):
+    print('Buzz')
+  else:
+    print(number)
+
+
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-5-4-exercise#main.py
