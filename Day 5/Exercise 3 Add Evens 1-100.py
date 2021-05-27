ad8cabad70feb31c7cc29bbc3e039768f1caa823
Andrew Ash <lludu@live.com>
2021-05-27 10:21:39 -0400

    Day 005: Exercise 3, Add Evens 1-100

---
+++ Day 5/Exercise 3 Add Evens 1-100.py
@@ -0,0 +1,13 @@
+#This is day 5, python loops
+
+#Exercise 2 - Adding all even numbers between 1 to 100:
+
+
+total = 0
+for number in range(0,101,2):
+  total += number
+print(f'the sum of all the even numbers from 1 to 100 is: {total}')
+
+
+#Link to Exercise
+# https://replit.com/@Lludu/day-5-3-exercise#main.py
